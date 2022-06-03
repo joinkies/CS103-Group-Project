e9386b9345078a1ad573fffada9b5192007dc75a
jonks <48509712+joinkies@users.noreply.github.com>
2022-06-03 16:45:34 +1200

    barebones

--- CS103 Group Project/main.cpp
+++ CS103 Group Project/main.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
 using namespace std;
 
+void userReg();
+void driverReg();
+void transLog();
+void lostFound();
+void complaints();
+void help(int help_where);
+void updateUser();
+
 int main() {
 
+
+
+	return 0;
 }
